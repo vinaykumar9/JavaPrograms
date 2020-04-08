a73470c4f8e722bf7ba62a42ad539229170c3a7a
vinaykumar9 <gvinaykumar434@gmail.com>
2020-04-08 15:52:20 +0530

    new methods added

--- src/com/programs/Strings/StringDemo.java
+++ src/com/programs/Strings/StringDemo.java
@@ -33,6 +33,134 @@ public class StringDemo {
     System.out.println("equal ignore case");
    
     System.out.println(data1.equalsIgnoreCase(data4));
+    
+    
+    // == equal to operation
+    String s4= "kumar";
+    String s5= "kumar";
+    System.out.println(" ==case");
+
+    System.out.println(s4==s5);
+   
+    
+    //concat operation
+    
+    String s6 ="ramu";
+    System.out.println("concat operation..............");
+    System.out.println(s6.concat("kumar"));
+   // (+) operator ...........................
+    
+    System.out.println("(+) operator..............");
+    String s7 ="ramarao";
+    
+    System.out.println(s7+"babu");
+    
+    
+    //trim method 
+    System.out.println("trim operation");
+    
+    String s8 ="babu   ";
+    
+    
+   System.out.println(s8.trim());
+   
+   
+   //replace all
+   
+   System.out.println("replace all  method");
+   
+   
+   String s9 = "my name is babu and babu and babu";
+   s9 =s9.replaceAll("babu", "reddy");
+   
+   System.out.println(s9);
+   
+   
+   //split
+   System.out.println("split functionality...................");
+   
+   String s10 = "subburfgban and ban anad aban";
+   String[] s1= s10.split("ban");
+   
+   for(String g:s1){
+	   System.out.println(g);
+   }
+   
+   //index of 
+   
+   
+   System.out.println("index of operations");
+   
+   
+   String s11 = "my name is vinay";
+   
+   System.out.println(s11.indexOf('n'));
+   
+   //string reverse
+   
+   System.out.println("String revrsal program..........");
+   
+   String babu ="vinaybabu";
+   String rev= "";
+   int length =babu.length();
+   for(int i =length-1;i>=0;i--){
+	   rev = rev+babu.charAt(i);
+   }
+   
+   System.out.println(rev);
+   
+   //polindrome program
+   
+   
+   System.out.println("string polindrome program...........");
+   
+  //uperscase
+   
+   System.out.println("uppercase letters............");
+   
+   String data100 = "vinay";
+   System.out.println(data100.toUpperCase());
+   
+   
+   
+  //lower case
+   
+   System.out.println("LOwer case letters............");
+   
+   String data101 = "FVLJLJDFL";
+   System.out.println(data101.toLowerCase());
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   		
+   
+   
+   
+   
+   
+   
+   
+    
+ 
+    
+    
+    
+    
+    
+    
+    
 
 
     
